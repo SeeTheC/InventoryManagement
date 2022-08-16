7ca184ae2df04a4763c55568e462d83e61faf6f2
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-16 13:54:21 +0800

    Delete resources directory

--- resources/views/im/layout/footer.php
+++
@@ -1,7 +0,0 @@
-		</div> <!-- End of content-row2 -->       
-    <div class="col-sm-2"> <!-- Right Pan--> 
-    </div> 
-  </div> <!-- End of content -->
-</div> <!-- End of container -->
-</body>
-</html>
